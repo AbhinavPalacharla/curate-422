06dc5367a52240767109bf3bc57b59b5cacf81ab
Abhinav Palacharla <abhinav.palacharla@gmail.com>
2024-05-29 19:01:23 -0700

    ✨ UI for artifacts menu

--- src/components/artifacts/Artifact.tsx
+++ src/components/artifacts/Artifact.tsx
@@ -2,16 +2,36 @@ import Image from "next/image";
 import Link from "next/link";
 import type { ArtifactMedia } from "@prisma/client";
 import { PostMetaInfo } from "./PostMetaInfo";
+import { Dropdown } from "../shared/Dropdown";
+import { getIconByName } from "@/utils";
 
 const Artifact: React.FC<{
   media: Array<ArtifactMedia>;
   description: string;
   createdAt: Date;
 }> = ({ media, description, createdAt }) => {
+  media = Array(3).fill(media[0]);
   return (
     <div className="border-[#292929] border-[1px] rounded-lg lg:rounded-md p-2 lg:p-4 lg:pb-6 mb-4">
+      <Dropdown
+        items={[
+          {
+            name: "Move to ...",
+            icon: getIconByName({ name: "ArrowRightCircle" }),
+          },
+          { name: "Copy to ...", icon: getIconByName({ name: "Copy" }) },
+          // { name: "Delete", icon: getIconByName({ name: "Trash" }) },
+        ]}
+        action={{ name: "Delete", onClick: () => {} }}
+      >
+        <button
+          className={`text-[#969696] hover:text-white tracking-wide border-[1px] border-[#282828] w-7 h-7 rounded-md`}
+        >
+          ···
+        </button>
+      </Dropdown>
       {media && (
-        <>
+        <div className="pt-4">
           {media.length === 1 ? (
             <div className="flex flex-row items-center justify-center">
               <Image
@@ -25,11 +45,11 @@ const Artifact: React.FC<{
             </div>
           ) : (
             <div className="flex flex-row overflow-x-scroll scroll-smooth gap-x-4 scrollbar-hide">
-              {media.map((media) => (
+              {media.map((m) => (
                 <Image
-                  key={media.imageURL}
+                  key={m.imageURL}
                   alt={"Image"}
-                  src={media.imageURL}
+                  src={m.imageURL}
                   width={4096}
                   height={4096}
                   className="h-96 max-w-none w-auto rounded-sm"
@@ -37,7 +57,7 @@ const Artifact: React.FC<{
               ))}
             </div>
           )}
-        </>
+        </div>
       )}
       <h1 className="text-white pt-4">{description}</h1>
       <PostMetaInfo date={createdAt} />
--- src/components/artifacts/PostMetaInfo.tsx
+++ src/components/artifacts/PostMetaInfo.tsx
@@ -1,4 +1,6 @@
 import { Roboto_Mono } from "next/font/google";
+import { Dropdown } from "@/components/shared/Dropdown";
+import { getIconByName } from "@/utils";
 
 const robotoMono = Roboto_Mono({
   weight: "variable",
@@ -9,12 +11,28 @@ const PostMetaInfo: React.FC<{
   date: Date;
 }> = ({ date }) => {
   return (
-    <div className="flex flex-row items-center pt-4 lg:pt-8 pb-2 lg:pb-0">
+    <div className="flex flex-row items-center justify-between pt-4 lg:pt-8 pb-2 lg:pb-0">
       <h1
         className={`${robotoMono.className} font-light text-sm text-[#969696] tracking-wide pl-1 lg:pl-0`}
       >
         {new Date(date).toLocaleDateString().replaceAll("/", ".")}
       </h1>
+      {/* <Dropdown
+        items={[
+          {
+            name: "Move to ...",
+            icon: getIconByName({ name: "ArrowRightCircle" }),
+          },
+          { name: "Copy to ...", icon: getIconByName({ name: "Copy" }) },
+          { name: "Delete", icon: getIconByName({ name: "Trash" }) },
+        ]}
+      >
+        <button
+          className={`text-[#969696] hover:text-white tracking-wide border-[1px] border-[#282828] w-7 h-7 rounded-md`}
+        >
+          ···
+        </button>
+      </Dropdown> */}
     </div>
   );
 };
